87d4a63b8aa6a871fc49e8973faeb07fe450b529
max.b <max.b@dev.izibook.ru>
2021-12-04 03:21:16 +0300

    Создание моковых данных и добаалеие их в шаблон

--- src/view/film-details-content-btn-view.js
+++ src/view/film-details-content-btn-view.js
@@ -1,4 +1,15 @@
-export const createControlBtnTemplate = (detailsControl, active) => {
+export const createControlBtnTemplate = (detailsControl, userDetails) => {
+  const adapterDate  = (details) => ({
+    favorite:details.favorite,
+    watched:details.alreadyWatched,
+    watchlist:details.watchlist,
+    watchingDate:details.watchingDate
+  });
+  let active = '';
+  const date = adapterDate(userDetails);
   const {styleClass, title, id, name} = detailsControl;
+  if(date[id]) {
+    active = 'film-details__control-button--active';
+  }
   return `<button type="button" class="film-details__control-button ${active} ${styleClass}" id="${id}" name="${name}">${title}</button>`;
 };
